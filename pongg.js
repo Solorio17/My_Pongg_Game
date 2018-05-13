86f77d56f137d3240fb4b503c1ea1bc15721a7dd
Jason <jasonsoloriom@gmail.com>
2018-05-13 14:14:05 -0600

    Creating paddles, adding new variables to hold position of x and another one to hold its speed, creating another function called objectMovements that will move the objects, ans seeting an interval that will handle running the functions at the bottom correctly

--- pongg.js
+++ pongg.js
@@ -1,10 +1,20 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
+var ballSpeedX = 5;
 
 window.onload = function(){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   drawEverything();
+  var framesPerSecond = 65;
+  setInterval(function(){objectMovements(); drawEverything()},
+  1000/framesPerSecond);
+}
+
+function objectMovements(){
+    ballX = ballX + ballSpeedX;
+    ballSpeedX = ballSpeedX + 1;
 }
 
 
@@ -13,5 +23,11 @@ function drawEverything(){
   canvasContext.fillRect(0,0, canvas.width,canvas.height);
 
   canvasContext.fillStyle = 'green';
-  canvasContext.fillRect(20,20,20,20);
+  canvasContext.fillRect(ballX,78,20,20);
+
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(6,300,15,110);
+
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(979,288,15,110);
 }
